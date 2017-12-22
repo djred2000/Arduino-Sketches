456fe62f9312cb0a4288d671e1406759591245e1
djred2000 <djred2000@gmail.com>
2017-12-22 08:41:58 -0500

    Updated for ESP32

--- Pixel_Controller/ESP8266/E131_Passthrough/E131.ino
+++ Pixel_Controller/ESP8266/E131_Passthrough/E131.ino
@@ -1,4 +1,3 @@
-//Simple check to verify data is E1.31. 
 int checkE131Headers(unsigned char* messagein, int messagelength) 
 {
   if ( messagein[1] == 0x10 && messagein[4] == 0x41 && messagein[12] == 0x37) 
