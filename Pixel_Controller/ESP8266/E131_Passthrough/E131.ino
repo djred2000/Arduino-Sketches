89ae22fd153b2f5908dbff8532649de8fbc41cfa
djred2000 <djred2000@gmail.com>
2019-01-26 15:22:03 -0500

    Add files via upload

---
+++ Pixel_Controller/ESP8266/E131_Passthrough/E131.ino
@@ -0,0 +1,9 @@
+int checkE131Headers(unsigned char* messagein, int messagelength) 
+{
+  if ( messagein[1] == 0x10 && messagein[4] == 0x41 && messagein[12] == 0x37) 
+  {  
+      int ChannelCount = messagein[123] * 256 + messagein[124]; // number of values plus start code
+      return ChannelCount -1; //Return how many values are in the packet.
+  }
+  return 0;
+}
